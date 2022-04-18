0a5caf53efec9768f970031f7db770c2b628838c
buftoronto <andy.weihuang@gmail.com>
2022-04-18 16:49:14 -0400

    add data import lib

---
+++ DataImport/CsvReader.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataImport
+{
+   public class CsvReader
+   {
+   }
+}
---
+++ DataImport/ValidationAttributes/HasItems.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImport.ValidationAttributes
+{
+   [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+
+   public sealed class HasItems : ValidationAttribute
+   {
+      public override bool IsValid(object value)
+      {
+         if (value == null)
+         {
+            return true;
+         }
+         else if (value is ICollection<object>)
+         {
+            return ((ICollection<object>)value).Count > 0;
+         }
+         else if (value is IEnumerable<object>)
+         {
+            return ((IEnumerable<object>)value).Count() > 0;
+         }
+         else
+         {
+            return true;
+         }
+
+      }
+   }
+}
