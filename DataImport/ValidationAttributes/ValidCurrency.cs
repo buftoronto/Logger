44035eefee765ccc329405378ff6731eedfadd31
buftoronto <andy.weihuang@gmail.com>
2022-04-18 16:57:31 -0400

    Update ValidCurrency.cs

--- DataImport/ValidationAttributes/ValidCurrency.cs
+++ DataImport/ValidationAttributes/ValidCurrency.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace DataImport.ValidationAttributes
 {
-   class ValidCurrency
-   {
-   }
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public sealed class ValidCurrency : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            string strVal = (value as string);
+
+            if (strVal == null)
+            {
+                return true;
+            }
+
+            strVal = strVal.Replace("$", string.Empty).Trim();
+
+            if (string.IsNullOrEmpty(strVal))
+            {
+                return true;
+            }
+            else
+            {
+                decimal toss;
+                return decimal.TryParse(strVal, out toss);
+            }
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format("Field: [{0}] can not be parsed to a valid currency type.", name);
+        }
+    }
 }
