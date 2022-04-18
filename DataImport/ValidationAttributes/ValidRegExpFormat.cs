deab7ff65e598e4c991e05e01fd39d9f1df9a819
buftoronto <andy.weihuang@gmail.com>
2022-04-18 16:59:24 -0400

    Update ValidRegExpFormat.cs

--- DataImport/ValidationAttributes/ValidRegExpFormat.cs
+++ DataImport/ValidationAttributes/ValidRegExpFormat.cs
@@ -7,8 +7,34 @@ using System.Threading.Tasks;
 
 namespace DataImport.ValidationAttributes
 {
-   [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
-   public sealed class ValidRegExpFormat : ValidationAttribute
-   {
-   }
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    sealed public class ValidSelectOption : ValidationAttribute
+    {
+        private string strVal;
+        private static Regex requiredEx = new Regex(@"(^\d{2}.*$)|(^\s*$)", RegexOptions.Compiled);
+        private static Regex notRequiredEx = new Regex(@"(^\d{1,2}.*$)|(^\s*$)", RegexOptions.Compiled);
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            strVal = (string)value;
+            if (ValidationConfig.NeedRequiredConfig)
+            {
+                return requiredEx.IsMatch(strVal);
+            }
+            else
+            {
+                return notRequiredEx.IsMatch(strVal);
+            }
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format("The {0} field could not be parsed to a valid Select Field.", name);
+        }
+    }
 }
